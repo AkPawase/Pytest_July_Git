9f2b9b69a96468ab716f72429f7effbe092dd9b3
AkPawase <akashpawase@gmail.com>
2023-07-31 00:08:25 +0530

    First Commit File

---
+++ Test_sum.py
@@ -0,0 +1,14 @@
+
+
+
+class Test_Addition_01:
+
+    def test_sum_001(self):
+        a = 3
+        b = 7
+        sum = a + b
+        print("Sum of a and b is :" + str(sum))
+        if sum == 10:
+            assert True
+        else:
+            assert False
